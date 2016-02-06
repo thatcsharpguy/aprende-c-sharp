5c8f9cd9a0dfcff8b22392a5e4729cbae729c760
Antonio Feregrino <antonio.feregrino@gmail.com>
2016-02-06 07:49:00 -0600

    LinqPost3 :)

---
+++ linq-en-c-sharp-3/LinqPost3/Lecture.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace LinqPost3
+{
+	public class Lecture
+	{
+		public int Id { get; set; }
+
+		public int TeacherId { get; set; }
+
+		public string Name { get; set; }
+	}
+}
+
---
+++ linq-en-c-sharp-3/LinqPost3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace LinqPost3
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			var db = new FakeDatabase ();
+
+			#region Grouping
+
+			var classesPerTeacher = from lecture in db.Lectures
+				group lecture by lecture.TeacherId into groupedLectures
+				select new { TeacherId = groupedLectures.Key, LectureCount = groupedLectures.Count() };
+
+			foreach(var item in classesPerTeacher.OrderBy(t=>t.LectureCount))
+			{
+				Console.WriteLine(item.TeacherId + "\t" + item.LectureCount);
+			}
+
+
+			#endregion
+
+			Console.Read ();
+		}
+	}
+}
