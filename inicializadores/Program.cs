9465a28120bd2636a29065224799d1b4649fdaf1
Antonio Feregrino <antonio.feregrino@gmail.com>
2016-03-10 20:10:59 -0600

    INicializadores

---
+++ inicializadores/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Inicializadores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    class Person
+    {
+        public Person(string name, Person dad, Person mom)
+        {
+            Name = name;
+            Dad = dad;
+            Mom = mom;
+        }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public Person Dad { get; set; }
+        public Person Mom { get; set; }
+        public List<Person> Friends { get; set; }
+    }
+}
