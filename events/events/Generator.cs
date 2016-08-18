2c1e26294d502ece23e0770e7b3a0b1003601ba4
Antonio Feregrino <antonio.feregrino@gmail.com>
2016-08-17 19:59:29 -0500

    Events

---
+++ events/events/Generator.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Events
+{
+
+
+	public class Generator 
+	{
+
+		Random r;
+
+		public bool IsRunning { get; private set; }
+
+		public string Name { get; set; }
+
+		public Generator(string name)
+		{
+			Name = name;
+			r = new Random(name.GetHashCode());
+		}
+
+
+		public delegate void GeneratingNumberEventHandler(Generator sender);
+
+		public event GeneratingNumberEventHandler GeneratingNumber;
+
+		public event EventHandler<int> EvenNumberGenerated;
+
+		public Action<int> EvenNumberGeneratedAction;
+
+		public void Start()
+		{
+			var t = new Thread(() =>
+			{
+			IsRunning = true;
+			while (IsRunning)
+			{
+				Thread.Sleep(500);
+
+					if (GeneratingNumber!= null)
+					{
+						GeneratingNumber(this);
+					}
+					var generated = r.Next();
+
+					if (generated % 2 == 0)
+					{
+						if (EvenNumberGenerated != null)
+						{
+							EvenNumberGenerated(generated, generated);
+						}
+
+						if (EvenNumberGeneratedAction != null)
+						{
+							EvenNumberGeneratedAction(generated);
+						}
+					}
+					//Console.WriteLine(generated);
+				}
+			});
+
+			t.Start();
+		}
+
+
+		public void Stop()
+		{
+			IsRunning = false;
+		}
+	}
+}
+
+
---
+++ events/events/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace Events
+{
+	class MainClass
+	{
+		static Generator g;
+
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Escribe un nombre y presiona enter");
+			var name = Console.ReadLine();
+
+			g = new Generator(name);
+
+
+			g.GeneratingNumber += G_GeneratingNumber;
+
+			g.EvenNumberGenerated += (sender, number) => 
+			{
+				Console.WriteLine("Se generó el número: " + number + " (manejador 1)");
+			};
+
+			g.EvenNumberGeneratedAction += (obj) => 
+			{  
+				Console.WriteLine("Se generó el número: " + obj + " (Action 1)");
+			};
+
+			//g.EvenNumberGenerated(5);
+			g.EvenNumberGeneratedAction(3);
+
+			//g.EvenNumberGenerated = null;
+			g.EvenNumberGeneratedAction = null;
+
+
+			g.EvenNumberGenerated += G_EvenNumberGenerated;
+
+			g.Start();
+
+			System.Threading.Thread.Sleep(TimeSpan.FromMinutes(1));
+		}
+
+		static void G_GeneratingNumber(Generator sender)
+		{
+			var myGenerator = sender as Generator;
+			Console.WriteLine(myGenerator.Name + " generará un número");
+		}
+
+		static void G_EvenNumberGenerated(object sender, int number)
+		{
+			if (number % 3 == 0)
+			{
+				Console.WriteLine("Se generó el número: " + number + "  que también es multiplo de 3, bye bye!");
+				g.EvenNumberGenerated -= G_EvenNumberGenerated;
+			}
+		}
+	}
+}
