7e09df5d2dd4398c57b2be0d0a90343211ab3741
Antonio Feregrino <antonio.feregrino@gmail.com>
2016-09-05 17:59:15 -0500

    Attrs

---
+++ Attributes/Nokia3310.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.CompilerServices;
+
+namespace Attributes
+{
+	[ObsoleteAttribute]
+	public class Nokia3310
+	{
+		[RangeAttribute(0,9990.99)]
+		public double Balance { get; set; }
+		
+		[Obsolete]
+		public void Call(string number, [CallerMemberName]string whoIsCalling = null)
+		{
+			Console.WriteLine(whoIsCalling + " is calling " + number);
+		}
+	}
+}
+
---
+++ Attributes/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Attributes
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			var phone = new Nokia3310();
+		}
+	}
+}
