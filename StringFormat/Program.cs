2c2b16116d00dc65f488609c5995ccb7e6e6a9ea
Antonio Feregrino <hs_mac@MacBook-Pro-de-Antonio.local>
2016-10-24 14:04:12 -0500

    Samples.

--- StringFormat/Program.cs
+++ StringFormat/Program.cs
@@ -13,6 +13,7 @@ namespace StringFormat
 			Console.WriteLine(String.Format("{2}, {0} y {1}", "uno", "dos", "tres"));
 			Console.WriteLine();
 
+
 			Console.WriteLine("Integer formatting");
 			int[] enteros = { 1300, -52, 0 };
 			FormatArray("{0}", enteros);
@@ -22,39 +23,38 @@ namespace StringFormat
 			FormatArray("{0:#.00;Minus 0;Nothing}", enteros);
 			Console.WriteLine();
 
+
 			Console.WriteLine("Hexadecimal:");
 			int r = 0, g = 133, b = 20;
 			Console.WriteLine(String.Format("Hex: R{0:x}, G{1:x4}, B{2:x2}", r, g, b));
 			Console.WriteLine("Hex: {0:x2}{1:x2}{2:x2}", r, g, b);
 			Console.WriteLine();
 
-			Console.WriteLine("Paddings:");
-			FormatArray("Padded numbers: {0,10}", enteros);
-			FormatArray("Padded numbers: {0,10:00000}", enteros);
-			FormatArray("{0,10}|{0,-10}", enteros);
 
-			var data = new[]
-			{
-				new { Dato1 = 0, Dato2="México", Dato3 = DateTime.Now },
-				new { Dato1 = 2, Dato2 = "Canadá", Dato3 = DateTime.Now.AddDays(3) },
-				new { Dato1 = 10, Dato2 = "Panamá", Dato3 = DateTime.Now.AddDays(-2) },
-				new { Dato1 = 0, Dato2 = "Perú", Dato3 = DateTime.Now.AddMonths(-2) }
-			};
-			foreach (var item in data)
-			{
-				Console.WriteLine("{0,10:000}|{1,10}|{2,10:dd-MM}", item.Dato1, item.Dato2, item.Dato3);
-			}
-
-			Console.WriteLine();
-
-
-			Console.WriteLine("Double formatting:");
-			double[] dobles = { 13000, -52.0, 0.0 };
+			Console.WriteLine("Real numbers formatting:");
+			double[] dobles = { 13000, -52.0d, 0.0, 0.6852871999174d };
+			float[] flotantes = { 0.13f, -0.50f, 0.0f, 0.6852871999174f };
+			decimal[] decimales = { 0.13m, -0.50m, 0.0m, 0.6852871999174m };
 			FormatArray("{0:r}", dobles);
 			FormatArray("{0:#,##0.00}", dobles);
-			FormatArray("{0:#,##0.00;;—}", dobles);
-			FormatArray("{0:0%}", dobles);
+			FormatArray("{0:0.0%}", dobles);
+			FormatArray("{0:r}", flotantes);
+			FormatArray("{0:#,##0.00}", flotantes);
+			FormatArray("{0:0.0%}", flotantes);
+			FormatArray("{0:r}", decimales);
+			FormatArray("{0:#,##0.00}", decimales);
+			FormatArray("{0:0.0%}", decimales);
 			Console.WriteLine();
+
+
+			Console.WriteLine("Conditional formatting:");
+			FormatArray("{0:#,##0.00;MENOS #,##0.00;—}", decimales);
+
+			Console.WriteLine(String.Format("{0:VERDADERO;FALSO;DESCONOCIDO}", 1)); // Imprime: "VERDADERO"
+			Console.WriteLine(String.Format("{0:VERDADERO;FALSO;DESCONOCIDO}", 0));  // Imprime: "DESCONOCIDO"
+			Console.WriteLine(String.Format("{0:VERDADERO;FALSO;DESCONOCIDO}", -1));  // Imprime: "FALSO"
+			Console.WriteLine();
+
 
 			Console.WriteLine("DateTime formatting:");
 			var now = DateTime.Now;
@@ -72,8 +72,27 @@ namespace StringFormat
 						"{0:dd/MM/yyyy}",
 						"{0:dd-MM-yyyy}",
 						"{0:dd MMM yyyy}",
-			            "{0:dd MMMM yy}");
+						"{0:dd 'de' MMMM 'de' yy}");
+			Console.WriteLine();
 
+
+			Console.WriteLine("Paddings:");
+			FormatArray("Padded numbers: {0,10}", enteros);
+			FormatArray("Padded numbers: {0,10:00000}", enteros);
+			FormatArray("Izq|{0,10}|{0,-10}|Der", enteros);
+
+			var data = new[]
+			{
+				new { Dato1 = 0, Dato2="México", Dato3 = DateTime.Now },
+				new { Dato1 = 2, Dato2 = "Canadá", Dato3 = DateTime.Now.AddDays(3) },
+				new { Dato1 = 10, Dato2 = "Panamá", Dato3 = DateTime.Now.AddDays(-2) },
+				new { Dato1 = 0, Dato2 = "Perú", Dato3 = DateTime.Now.AddMonths(-2) }
+			};
+			foreach (var item in data)
+			{
+				Console.WriteLine("|{0,10:000}|{1,10}|{2,10:dd-MM}|", item.Dato1, item.Dato2, item.Dato3);
+			}
+			Console.WriteLine();
 		}
 
 		public static void MultiFormat<T>(T value, params string [] formats)
@@ -86,7 +105,7 @@ namespace StringFormat
 
 		public static void FormatArray<T>(string format, params T[] array)
 		{ 
-			Console.WriteLine("Format: " + format);
+			Console.WriteLine("Format " + typeof(T).Name+ ": " + format);
 			foreach (var item in array)
 			{
 				try
@@ -102,3 +121,5 @@ namespace StringFormat
 
 	}
 }
+
+
