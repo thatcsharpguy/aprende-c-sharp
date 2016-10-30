11aa5188304d57400ec3baef376d4866564e01c1
Antonio Feregrino <antonio.feregrino@gmail.com>
2016-10-30 11:35:55 -0600

    DBtips

---
+++ DebuggingTips/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace DebuggingTips
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+#if DEBUG
+			Console.WriteLine("Solo visible en modo debug, directiva: DEBUG");
+#endif
+
+			var tc = new TestClass() { Propiedad1 = "Uno, dos y tres" };
+			Console.WriteLine(tc);
+
+			Console.WriteLine("Hello World!");
+		}
+	}
+}
---
+++ DebuggingTips/TestClass.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace DebuggingTips
+{
+	[DebuggerDisplay("Propiedad1: {Propiedad1}")]
+	public class TestClass
+	{
+		public string Propiedad1 { get; set; }
+		public int Propiedad2 { get; set; }
+
+		public TestClass()
+		{
+		}
+
+		//public override string ToString()
+		//{
+		//	return string.Format("{0}: {1}", nameof(Propiedad1), Propiedad1);
+		//}
+	}
+}
