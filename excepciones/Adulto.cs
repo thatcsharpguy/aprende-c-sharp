fbe141926db964876d747de4111264eea497cc44
Antonio Feregrino <antonio.feregrino@gmail.com>
2017-06-10 21:45:12 -0500

    Ejemplo completo.

---
+++ excepciones/Adulto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace Excepciones
+{
+    public class Adulto
+    {
+        public Adulto()
+        {
+        }
+
+        private int _numeroGatos;
+        public int NumeroGatos
+        {
+            get { return _numeroGatos; }
+            set
+            {
+                if (value > 4)
+                    throw new TienesMuchosGatosException(value);
+                _numeroGatos = value;
+            }
+        } 
+    }
+}
--- excepciones/Program.cs
+++ excepciones/Program.cs
@@ -10,6 +10,69 @@ namespace Excepciones
     {
         static void Main(string[] args)
         {
+            /// ### Por razones evidentes, mucho del 
+            /// código de este demo está comentado ###
+
+            //throw new Exception("Hey, soy el mensaje de la excepción");
+
+            //var ex = new FormatException("Hey, soy una excepción guardada en una variable");
+            //throw ex;
+
+            //var adulto = new Adulto();
+            //adulto.NumeroGatos = 10;
+
+            var adulto2 = new Adulto();
+            try
+            {
+                // Intentamos algo peligroso:
+                adulto2.NumeroGatos = Int32.MaxValue;
+            }
+            catch (TienesMuchosGatosException te)
+            {
+                // Manejamos la excepción relacionada con los gatos:
+                Console.WriteLine("Ooops, no puedes tener " + te.NumeroGatos + " gatos");
+            }
+            catch(NullReferenceException ne)
+            {
+                // Maejamos el error relacionado con objetos nulos
+            } 
+            catch(ArithmeticException)
+            {
+                // Manejamos cualquier error relacionad 
+                // con desborde de números o divisiones entre 0
+            }
+            catch
+            {
+                // Manejamos cualquier otra excepción
+            }
+
+            RelanzaExcepcion();
+        }
+
+        private static void RelanzaExcepcion()
+        {
+            try
+            {
+                throw new NotImplementedException();
+            }
+            catch(Exception e)
+            {
+                // Logging
+                //throw e; // ¡No lo hagas, es malo!
+                throw;
+            }
+        }
+    }
+
+    public class TienesMuchosGatosException : Exception
+    {
+
+        public int NumeroGatos { get; private set; }
+
+        public TienesMuchosGatosException(int numeroGatos)
+            : base("¡" + numeroGatos + " son muchos gatos!")
+        {
+            NumeroGatos = numeroGatos;
         }
     }
 }
